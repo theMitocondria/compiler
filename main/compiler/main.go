ca75653370c18e49e1f4f87669299e8c95137724
Gautam Khatri <buddhgautam777@gmail.com>
2024-12-11 16:06:10 +0530

    ✨ gracefull shutdown

--- main/compiler/main.go
+++ main/compiler/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/theMitocondria/compiler/internal/config"
 )
@@ -21,19 +26,41 @@ func main() {
 		w.Write([]byte("compiled successfully."))
 	})
 
+	//setup server
+
 	server := http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
 
-	err := server.ListenAndServe()
+	slog.Info("server stared", slog.String("address: ", cfg.Addr))
 
-	if err != nil {
-		log.Fatal("failed to start server")
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+
+
+	slog.Info("shutting down the server gracefully")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 4* time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
 	}
 
-	slog.Info("Server Started")
+	slog.Info("server shutdown successfully")
 
-	//setup server
+
 
 }
