77db1ca5a29b993c677a0bb0457a8bc14334601c
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-12-15 21:59:02 +0530

    feat: :sparkles: add fiber

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/PuerkitoBio/purell v1.2.1 // indirect
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/bytedance/sonic v1.12.5 // indirect
 	github.com/bytedance/sonic/loader v0.2.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
@@ -23,22 +24,31 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.23.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/gofiber/fiber/v2 v2.52.5 // indirect
+	github.com/google/uuid v1.5.0 // indirect
 	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
 	github.com/swaggo/gin-swagger v1.6.0 // indirect
 	github.com/swaggo/swag v1.16.4 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/arch v0.12.0 // indirect
 	golang.org/x/crypto v0.30.0 // indirect
 	golang.org/x/net v0.32.0 // indirect
--- main/main.go
+++ main/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"context"
-	"log"
 	"log/slog"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"github.com/theMitocondria/compiler/internal/http/routers/compile"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	spincontainers "github.com/theMitocondria/compiler/internal/utils/spinContainers"
 )
 
@@ -22,40 +16,47 @@ func main() {
 		slog.Info("containers spined")
 	}()
 	//setup router
-	router := compile.InitRouter()
+	// router := compile.InitRouter()
 
-	//setup server
+	// //setup server
 
-	server := http.Server{
-		Addr:    "localhost:3000",
-		Handler: router,
-	}
+	// server := http.Server{
+	// 	Addr:    "localhost:3000",
+	// 	Handler: router,
+	// }
 
-	slog.Info("server stared")
+	// slog.Info("server stared")
 
-	done := make(chan os.Signal, 1)
+	// done := make(chan os.Signal, 1)
 
-	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	// signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	go func() {
-		err := server.ListenAndServe()
+	// go func() {
+	// 	err := server.ListenAndServe()
 
-		if err != nil {
-			log.Fatal("failed to start server")
-		}
-	}()
+	// 	if err != nil {
+	// 		log.Fatal("failed to start server")
+	// 	}
+	// }()
 
-	<-done
+	// <-done
 
-	slog.Info("shutting down the server gracefully")
+	// slog.Info("shutting down the server gracefully")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
-	defer cancel()
+	// ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+	// defer cancel()
 
-	if err := server.Shutdown(ctx); err != nil {
-		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
-	}
+	// if err := server.Shutdown(ctx); err != nil {
+	// 	slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	// }
 
-	slog.Info("server shutdown successfully")
+	// slog.Info("server shutdown successfully")
 
+	app := fiber.New()
+
+	app.Use(logger.New())
+
+	group := app.Group("/api")
+
+	app.Listen(":3000")
 }
